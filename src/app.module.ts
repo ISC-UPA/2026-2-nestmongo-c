7494a989662baabe8ffcd36ed9c860f67b823baf
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-07-20 18:58:17 -0600

    Auto 2026-07-20 18:58

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DepartamentosModule } from './modules/departamentos/departamentos.module';
+import { HealthController } from './controllers/health/health.controller';
+import { DepartamentosController } from './controllers/departamentos/departamentos.controller';
+import { DepartamentosService } from './services/departamentos/departamentos.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DepartamentosModule],
+  controllers: [AppController, HealthController, DepartamentosController],
+  providers: [AppService, DepartamentosService],
 })
 export class AppModule {}
