efcff8314f3a492111542af1a3189ec5304f75b8
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-07-22 17:47:37 -0600

    Auto 2026-07-22 17:47

--- src/modules/departamentos.module.ts
+++ src/modules/departamentos.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DepartamentosController } from '../controllers/departamentos.controller';
+import { DepartamentosService } from '../services/departamentos.service';
+//import { Departamento } from '../schemas/departamento.schema';
+//import { DepartamentoSchema } from '../schemas/departamento.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Departamento.name, schema: DepartamentoSchema },
+    ]),
+  ],
+  controllers: [DepartamentosController],
+  providers: [DepartamentosService],
+})
+
 export class DepartamentosModule {}
